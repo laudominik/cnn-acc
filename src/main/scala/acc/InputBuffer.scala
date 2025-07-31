a6ca70b58cdb66724eedcdbfb1757b5216f0389c
Dominik Lau <harek2388@gmail.com>
2025-07-31 17:06:39 +0200

    add InputBuffer

---
+++ src/main/scala/acc/InputBuffer.scala
@@ -0,0 +1,34 @@
+import chisel3._
+import chisel3.util._
+
+import chisel3._
+import chisel3.util._
+
+class InputBuffer(val numBanks: Int, val bankDepth: Int, val width: Int) extends Module {
+  val addrWidth = log2Ceil(bankDepth)
+
+  val io = IO(new Bundle {
+    val wrEn   = Input(Vec(numBanks, Bool()))
+    val wrAddr = Input(Vec(numBanks, UInt(addrWidth.W)))
+    val wrData = Input(Vec(numBanks, UInt(width.W)))
+
+    val rdEn   = Input(Vec(numBanks, Bool()))
+    val rdAddr = Input(Vec(numBanks, UInt(addrWidth.W)))
+    val rdData = Output(Vec(numBanks, UInt(width.W)))
+  })
+
+  val readData = Wire(Vec(numBanks, UInt(width.W)))
+  val banks = Seq.fill(numBanks) {
+    SyncReadMem(bankDepth, UInt(width.W))
+  }
+
+  for (i <- 0 until numBanks) {
+    when (io.wrEn(i)) {
+      banks(i).write(io.wrAddr(i), io.wrData(i))
+    }
+  }
+  for (i <- 0 until numBanks) {
+    readData(i) := banks(i).read(io.rdAddr(i), io.rdEn(i))
+  }
+  io.rdData := readData
+}
---
+++ src/test/scala/acc/InputBufferSpec.scala
@@ -0,0 +1,50 @@
+import chisel3._
+import chisel3.simulator.scalatest.ChiselSim
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+
+class InputBufferSpec extends AnyFreeSpec with Matchers with ChiselSim {
+  "InputBuffer should correctly write and read multiple banks" in {
+    simulate(new InputBuffer(numBanks = 4, bankDepth = 8, width = 16)) { dut =>      
+      for (i <- 0 until 4) {
+        dut.io.wrEn(i).poke(false.B)
+        dut.io.wrAddr(i).poke(0.U)
+        dut.io.wrData(i).poke(0.U)
+        dut.io.rdEn(i).poke(false.B)
+        dut.io.rdAddr(i).poke(0.U)
+      }
+      dut.clock.step()
+
+      for (addr <- 0 until 8) {
+        for (bank <- 0 until 4) {
+          dut.io.wrEn(bank).poke(true.B)
+          dut.io.wrAddr(bank).poke(addr.U)
+          dut.io.wrData(bank).poke((bank * 1000 + addr).U)
+        }
+        dut.clock.step()
+      }
+
+      for (bank <- 0 until 4) {
+        dut.io.wrEn(bank).poke(false.B)
+      }
+      dut.clock.step()
+
+      for (addr <- 0 until 8) {
+        for (bank <- 0 until 4) {
+          dut.io.rdEn(bank).poke(true.B)
+          dut.io.rdAddr(bank).poke(addr.U)
+        }
+        dut.clock.step()
+
+        for (bank <- 0 until 4) {
+          val expected = bank * 1000 + addr
+          dut.io.rdData(bank).expect(expected.U)
+        }
+      }
+
+      for (bank <- 0 until 4) {
+        dut.io.rdEn(bank).poke(false.B)
+      }
+    }
+  }
+}
