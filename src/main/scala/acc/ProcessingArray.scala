1515fedbb394a8ba46f664aac1de1adc3b730424
Dominik Lau <harek2388@gmail.com>
2025-07-31 17:54:34 +0200

    add ProcessingArray

---
+++ src/main/scala/acc/ProcessingArray.scala
@@ -0,0 +1,33 @@
+import chisel3._
+import chisel3.util._
+
+// systolic array of MAcs
+// accumulation happens along the columns
+class ProcessingArray(rows: Int, cols: Int, width: Int = 8) extends Module {
+  val io = IO(new Bundle {
+    val inputs  = Input(Vec(cols, UInt(width.W)))
+    val weights = Input(Vec(rows, UInt(width.W)))
+    val out = Output(Vec(rows, UInt((2*width).W)))
+  })
+
+  val macs = Seq.fill(rows, cols)(Module(new MAc(width)))
+  
+  for (r <- 0 until rows) {
+    for (c <- 0 until cols) {
+      macs(r)(c).io.input := io.inputs(c)
+      macs(r)(c).io.weight := io.weights(r)
+    }
+  }
+  for (c <- 0 until cols) {
+    for (r <- 0 until rows) {
+      if (c == 0) {
+        macs(r)(c).io.accumulator := 0.U
+      } else {
+        macs(r)(c).io.accumulator := macs(r)(c - 1).io.out
+      }
+    }
+  }
+  for (r <- 0 until rows) {
+    io.out(r) := macs(r)(cols - 1).io.out
+  }
+}
---
+++ src/test/scala/acc/ProcessingArraySpec.scala
@@ -0,0 +1,42 @@
+import chisel3._
+import chisel3.experimental.BundleLiterals._
+import chisel3.simulator.scalatest.ChiselSim
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+
+class ProcessingArraySpec extends AnyFreeSpec with Matchers with ChiselSim {
+
+  "ProcessingArray should correctly accumulate sums along the columns" in {
+    val rows = 4
+    val cols = 5
+    val dataWidth = 8
+
+    simulate(new ProcessingArray(rows, cols, dataWidth)) { dut =>
+      dut.reset.poke(true.B)
+      dut.clock.step()
+      dut.reset.poke(false.B)
+      dut.clock.step()
+
+      val rand = new scala.util.Random
+      val inputs = Seq.fill(cols)(rand.nextInt(256).U(dataWidth.W))
+      val weights = Seq.fill(rows)(rand.nextInt(256).U(dataWidth.W))
+
+      for (r <- 0 until rows) {
+        dut.io.weights(r).poke(weights(r))
+      }
+      for (c <- 0 until cols) {
+        dut.io.inputs(c).poke(inputs(c))
+      }
+      dut.clock.step()
+
+      val expected = (0 until rows).map { r =>
+        ((0 until cols).map { c=>
+          inputs(c).litValue * weights(r).litValue
+        }).sum
+      }
+      for (r <- 0 until rows) {
+        dut.io.out(r).expect(expected(r).U(2 * dataWidth - 1, 0))
+      }
+    }
+  }
+}
